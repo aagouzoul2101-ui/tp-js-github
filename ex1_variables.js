e67356ba74bf0e588d0d1c29a277a47e9723a828
aagouzoul2101-ui <a.agouzoul2101@uca.ac.ma>
2026-02-27 16:29:50 +0000

    ajoue des exercice tp1 web2

--- ex1_variables.js
+++
@@ -1,9 +0,0 @@
-function infosEtudiant(nom, note) {
-    if (note >= 10) {
-        return nom + " : " + note + "/20 → Admis";
-    } else {
-        return nom + " : " + note + "/20 → Ajourné";
-    }
-}
-
-console.log(infosEtudiant("Hiba", 14));
